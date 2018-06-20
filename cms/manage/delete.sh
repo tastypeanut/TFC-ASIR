421252bd678c637857ad3d7f0f46babaae8a3d14
tastypeanut <tastypeanut@users.noreply.github.com>
2018-06-20 22:45:19 +0200

    changes

--- cms/manage/delete.sh
+++ cms/manage/delete.sh
@@ -10,29 +10,4 @@ sudo docker kill $2
 sudo docker rm -v $1
 sudo docker rm -v $2
 
-temp=$RANDOM.temp
-curl -X GET \
---header 'Accept: application/json' \
---header 'Authorization: Basic YXBpX3VzZXI6YXBpdXNlcg==' \
-'https://10.0.0.200:4444/api/objects/reverse_proxy/frontend/' -k -s > "$temp"
-var=$(wc -l "$temp" | cut -d " " -f 1)
-
-
-counter=0
-IFS=', ' read -r -a array <<< $(jq '.[].domain' "$temp" | grep  \"*\" | sed 's/\"//g')
-IFS=', ' read -r -a array2 <<< $(jq '.[]._ref' "$temp" | grep  \"*\" | sed 's/\"//g')
-for i in "${array[@]}"; do 
-	if [ $i = $3 ]; then
-		ref="${array2[$counter]}"
-	fi
-((counter++))
-done
-
-rm "$temp"
-
-curl -k -X DELETE --header 'Accept: application/json' \
---header 'X-Restd-Err-Ack: all' \
---header 'Authorization: Basic YXBpX3VzZXI6YXBpdXNlcg==' \
-"https://10.0.0.200:4444/api/objects/reverse_proxy/frontend/$ref"
-
 ssh -i "/opt/cms/keys/NGINXProxySSHKey.pem" ubuntu@10.0.1.200 "sudo rm /etc/nginx/sites-enabled/$database_container_name && sudo rm /etc/nginx/sites-available/$database_container_name && sudo service nginx reload"
